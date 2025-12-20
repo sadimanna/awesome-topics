cf889826e279366312b93576ae72354c4c75c964
sadimanna <manna.siladittya@gmail.com>
2025-12-20 20:19:53 +0530

    changed topicwise rendering

--- scripts/scaffold.py
+++ scripts/scaffold.py
@@ -73,9 +73,10 @@ class Scaffold:
                 year_block = venue_data[year]
                 year_id = f"{venue.lower()}-{year}"
 
-                md_lines.append(f'### {year} {{#{year_id}}}')
-                # md_lines.append(f'### {year}')
-                # md_lines.append(f'<a id="{year_id}"></a>')
+                # md_lines.append(f'### {year} {{#{year_id}}}')
+                md_lines.append(f'<a id="{year_id}"></a>')
+                md_lines.append(f'### {year}')
+                
                 md_lines.append(
                     utils.yaml_block_to_htmltable(
                         year_block["header"],
